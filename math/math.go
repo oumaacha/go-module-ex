7a25334dedac874276fbf8d77ba42cca757c842a
oumaacha <mr.anouar.oumaacha@gmail.com>
2023-05-26 14:44:02 +0100

    Adding math package

---
+++ math/math.go
@@ -0,0 +1,13 @@
+package math
+
+func addition(a int, b int) int {
+	return a+b
+}
+
+func soustraction(a int, b int) int {
+	return a-b
+}
+
+func multiplication(a int, b int) int {
+	return a*b
+}
